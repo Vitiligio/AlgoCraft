cc939ba1fc1ae4654caddd917dd8a30ca3a62547
sebastian <sebastian@sebastian-K52Dr>
2019-06-05 19:21:45 +0000

    tests

---
+++ test/model/TP2Tests.java
@@ -0,0 +1,60 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+
+public class TP2Tests {
+	
+	@Test
+	public void MaderaGolpeadaPorHachaDeMadera() {
+		
+	}
+	@Test
+	public void () {
+		
+	}
+	@Test
+	public void () {
+		
+	}
+	@Test
+	public void () {
+		
+	}
+	@Test
+	public void () {
+		
+	}
+	@Test
+	public void () {
+		
+	}
+	@Test
+	public void () {
+		
+	}
+	@Test
+	public void () {
+		
+	}
+	@Test
+	public void () {
+		
+	}
+	@Test
+	public void () {
+		
+	}
+	@Test
+	public void () {
+		
+	}
+	@Test
+	public void () {
+		
+	}
+	@Test
+	public void () {
+		
+	}
+
+}
