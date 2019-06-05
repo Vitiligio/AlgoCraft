46e4aa1cdb528439d64b73312694d5279243bcf0
sebastian <sebastian@sebastian-K52Dr>
2019-06-05 19:45:23 +0000

    sad

--- src/model/Material.java
+++ src/model/Material.java
@@ -3,11 +3,8 @@ package model;
 public abstract class Material {
 	abstract int durabilidadHacha();
 	abstract int durabilidadPico();
-	abstract int durabilidadPicoFino();
 	abstract int fuerzaHacha();
 	abstract int fuerzaPico();
-	abstract int fuerzaPicoFino();
 	abstract int tipoDeDesgastaHacha();
 	abstract int tipoDeDesgastePico();
-	abstract int tipoDeDesgastePicoFino();
 }
