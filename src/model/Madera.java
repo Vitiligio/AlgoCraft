5de7c0063d0930ce254e1c08ba153b5cdde8f173
sebastian <sebastian@sebastian-K52Dr>
2019-06-05 19:50:33 +0000

    Material madera

--- src/model/Madera.java
+++ src/model/Madera.java
@@ -1,5 +1,24 @@
 package model;
 
 public class Madera extends Material {
-
+	private int durabilidad = 10;
+	public int durabilidadHacha() {
+		return 100;
+	}
+	public int durabilidadPico() {
+		return 100;
+	}
+	
+	public int fuerzaHacha() {
+		return 2;
+	}
+	public int fuerzaPico() {
+		return 2;
+	}
+	public int tipoDeDesgastaHacha() {
+		return 0;
+	}
+	public int tipoDeDesgastePico() {
+		return 0;
+	}
 }
