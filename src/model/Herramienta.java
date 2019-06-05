cbc54b84da80019e4fdf745f2f002e2d965788eb
AlurrAldrey <aajoaco@gmail.com>
2019-06-05 16:35:11 -0300

    primer commit, estoy probando si anda

---
+++ src/model/Herramienta.java
@@ -0,0 +1,25 @@
+package model;
+
+public class Herramienta {
+
+		float durabilidad;
+		int fuerza;
+		Desgaste tipoDesgaste;
+		Material material
+		
+		public void usarContra(Madera madera) {
+			
+		}
+		
+		public void usarContra(Piedra piedra) {
+			
+		}
+		
+		public void usarContra(Metal metal) {
+			
+		}
+		
+		public void usarContra(Diamante diamante) {
+			
+		}
+}
--- src/model/Personaje.java
+++ src/model/Personaje.java
@@ -1,4 +1,14 @@
 package model;
 
 public class Personaje {
+	
+	float durabilidad;
+	int fuerza;
+	Desgaste desgaste;
+	Material material;
+	
+	public void usarContra(Madera madera) {
+	}
+	
+	public void usarContra(Piedra piedra)
 }
