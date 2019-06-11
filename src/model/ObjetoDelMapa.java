5f7a6ae75585177f8362049a6f05387b28a4e274
Sebastian Pagura <35182234+sebipagura@users.noreply.github.com>
2019-06-11 16:25:06 -0300

    Revert "objeto mapa"
    This reverts commit f9688e861c729674404cefc00c78a0ab285bc0cd.

--- src/model/ObjetoDelMapa.java
+++ src/model/ObjetoDelMapa.java
@@ -2,13 +2,7 @@ package model;
 
 public class ObjetoDelMapa {
 	Material contenido;
-	public ObjetoDelMapa() {
-		this.contenido = null;
-	}
-	public void ocupar(Material a_contener) {
+	public ObjetoDelMapa(Material a_contener) {
 		this.contenido = a_contener;
-	}
-	public Material getContenido() {
-		return contenido;
 	}
 }
