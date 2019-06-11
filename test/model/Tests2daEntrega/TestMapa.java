f360a11227e5261bd0f0a186472993fa47488166
sebastian <sebastian@sebastian-K52Dr>
2019-06-11 22:25:15 +0000

    test mapa

---
+++ test/model/Tests2daEntrega/TestMapa.java
@@ -0,0 +1,17 @@
+package model.Tests2daEntrega;
+
+import model.*;
+
+import org.junit.*;
+
+public class TestMapa {
+	
+	@Test
+	public void testInicializarJugador() {
+	}
+	
+	@Test
+	public void testMoverJugador() {
+		
+	}
+}
