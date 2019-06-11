3975dad9446ca15cc368df7dea0a50775be4775d
sebastian <sebastian@sebastian-K52Dr>
2019-06-11 23:44:46 +0000

    test jugador

--- test/model/Tests2daEntrega/TestJugador.java
+++ test/model/Tests2daEntrega/TestJugador.java
@@ -2,13 +2,18 @@ package model.Tests2daEntrega;
 
 import model.*;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.*;
 
 public class TestJugador {
 	
 	@Test
 	public void testSeInicializaJugadorEnElMapaCorrectamente() {
-		
+		GeneradorDeMapa generador = new GeneradorDeMapa();
+		int[] posicionJugador = {0,0};
+		Mapa mapa = new Mapa(generador.generarMapa(), posicionJugador);
+		assertEquals(mapa.posicionJugador(), posicionJugador);
 	}
 	
 	@Test
