9ee6ca0c3f3db711548df755b00189f38f4b90f1
sebastian <sebastian@sebastian-K52Dr>
2019-06-12 00:14:29 +0000

    mapa

--- src/model/Mapa.java
+++ src/model/Mapa.java
@@ -4,6 +4,7 @@ public class Mapa {
 	
 	Casillero[][] terreno = new Casillero[8][8];
 	int[] posicionJugador = new int[2];
+	Personaje jugador = new Personaje();
 	
 	public Mapa(Casillero[][] terreno, int[] posicionJugador) {
 	
@@ -19,6 +20,39 @@ public class Mapa {
 	
 	public Casillero verPosicion(int[] posicion) {
 		return terreno[posicion[0]][posicion[1]];
+	}
+	
+	private boolean moverJugadorA(int[] nueva_posicion) {
+		if (nueva_posicion[0] > 7)
+			return false;
+		if (nueva_posicion[0] < 0)
+			return false;
+		if (nueva_posicion[1] < 0)
+			return false;
+		if (nueva_posicion[1] > 7)
+			return false;
+		Casillero nuevo_casillero = terreno[nueva_posicion[0]][nueva_posicion[1]];
+		if (nuevo_casillero.obtenerContenido() != null)
+			return false;
+		nuevo_casillero.ocupar(jugador);
+		return true;
+	}
+	
+	public boolean moverJugadorIzquierda() {
+		int[] nueva_posicion = {posicionJugador[0], posicionJugador[1]-1};
+		return moverJugadorA(nueva_posicion);
+	}
+	public boolean moverJugadorDerecha() {
+		int[] nueva_posicion = {posicionJugador[0], posicionJugador[1]+1};
+		return moverJugadorA(nueva_posicion);
+	}
+	public boolean moverJugadorArriba() {
+		int[] nueva_posicion = {posicionJugador[0]-1, posicionJugador[1]};
+		return moverJugadorA(nueva_posicion);
+	}
+	public boolean moverJugadorAbajo() {
+		int[] nueva_posicion = {posicionJugador[0]+1, posicionJugador[1]};
+		return moverJugadorA(nueva_posicion);
 	}
 
 }
