e0b3432ecf4c879945f337f878aab89c753e53f0
sebastian <sebastian@sebastian-K52Dr>
2019-06-20 20:43:45 +0000

    ee

--- src/model/GeneradorDeMapa.java
+++ src/model/GeneradorDeMapa.java
@@ -7,14 +7,19 @@ public class GeneradorDeMapa {
 		for (int j = 0; j < 8; j++) {
 			casilleros[i][j] = new Casillero();
 		}
-		Madera madera = new Madera();
-		casilleros[1][0].ocupar(madera);
-		Piedra piedra = new Piedra();
-		casilleros[1][7].ocupar(piedra);
-		Metal metal = new Metal();
-		casilleros[2][7].ocupar(metal);
-		Diamante diamante = new Diamante();
-		casilleros[3][7].ocupar(diamante);
+		try {
+			Madera madera = new Madera();
+			casilleros[1][0].ocupar(madera);
+			Piedra piedra = new Piedra();
+			casilleros[1][7].ocupar(piedra);
+			Metal metal = new Metal();
+			casilleros[2][7].ocupar(metal);
+			Diamante diamante = new Diamante();
+			casilleros[3][7].ocupar(diamante);
+		}
+		catch (Exception e) {
+			
+		}
 		return casilleros;
 	}
 }
