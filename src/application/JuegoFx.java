93445501c1598cce5c57cef97e2cd50e54c8ea93
Usuario <Usuario@192.168.0.20>
2019-06-27 03:10:09 -0300

    Menu principal terminado

--- src/application/JuegoFx.java
+++ src/application/JuegoFx.java
@@ -13,11 +13,16 @@ import javafx.scene.layout.VBox;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.text.TextAlignment;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
-
+import javafx.scene.text.Font;
+import javafx.scene.layout.StackPane;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
 
 public class JuegoFx extends Application {
@@ -31,25 +36,39 @@ public class JuegoFx extends Application {
 	
 	public Scene menuPrincipal(Stage stage, HandlerEscenas handlerEscenas) {
 		
-		Text titulo = new Text("ALGOCRAFT");
+		/*Text titulo = new Text("ALGOCRAFT");
+		titulo.setFont(new Font("Verdana", 32));
 		titulo.setFill(Color.GREEN);
-		titulo.setStyle("-fx-font: 70px Tahoma");
 		HBox tituloBox = new HBox(titulo);
 		tituloBox.setAlignment(Pos.CENTER);
-		tituloBox.setTranslateY(0);
+		tituloBox.setTranslateY(0);*/
 		
-		Button jugar = new Button();
+		/*Button jugar = new Button();
 		jugar.setText("Jugar");
 		BotonJugarHandler handler_jugar = new BotonJugarHandler(stage, juego, handlerEscenas);
 		jugar.setOnAction(handler_jugar);
 		HBox jugarBox = new HBox(jugar);
 		jugarBox.setAlignment(Pos.CENTER);
-		jugarBox.setTranslateY(100);
+		jugarBox.setTranslateY(100);*/
+		
+		Image imagen = new Image("file:img/menu.png");
+		ImageView iv = new ImageView(imagen);
+		
+		StackPane pane = new StackPane();
+		pane.getChildren().add(iv);
 		
 		
-		VBox contenedorInicio = new VBox(tituloBox,jugarBox);
+		VBox contenedorInicio = new VBox(pane);
 		
-		Scene scene = new Scene(contenedorInicio,400,400);
+		Scene scene = new Scene(contenedorInicio,900,560);
+		scene.addEventFilter(KeyEvent.KEY_PRESSED, event->{
+			if(event.getCode() == KeyCode.ENTER) {
+				
+				Scene escenaMapa = new EscenaMapa(handlerEscenas).generarEscena(juego);
+				handlerEscenas.cambiarEscena(escenaMapa);
+				
+			}
+		});
 		return scene;
 	}
 	
