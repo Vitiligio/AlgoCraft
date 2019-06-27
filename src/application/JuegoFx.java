ce6f171bb457140c57fb805098edf702b0f83ee0
Usuario <Usuario@192.168.0.20>
2019-06-27 01:20:41 -0300

    cambios

--- src/application/JuegoFx.java
+++ src/application/JuegoFx.java
@@ -1,6 +1,7 @@
 package application;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
@@ -9,6 +10,15 @@ import handlers.*;
 import javafx.application.Application;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.TextAlignment;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+
+
 
 public class JuegoFx extends Application {
 	
@@ -20,12 +30,26 @@ public class JuegoFx extends Application {
 	}
 	
 	public Scene menuPrincipal(Stage stage, HandlerEscenas handlerEscenas) {
+		
+		Text titulo = new Text("ALGOCRAFT");
+		titulo.setFill(Color.GREEN);
+		titulo.setStyle("-fx-font: 70px Tahoma");
+		HBox tituloBox = new HBox(titulo);
+		tituloBox.setAlignment(Pos.CENTER);
+		tituloBox.setTranslateY(0);
+		
 		Button jugar = new Button();
 		jugar.setText("Jugar");
 		BotonJugarHandler handler_jugar = new BotonJugarHandler(stage, juego, handlerEscenas);
 		jugar.setOnAction(handler_jugar);
-		VBox contenedorInicio = new VBox(jugar);
-		Scene scene = new Scene(contenedorInicio);
+		HBox jugarBox = new HBox(jugar);
+		jugarBox.setAlignment(Pos.CENTER);
+		jugarBox.setTranslateY(100);
+		
+		
+		VBox contenedorInicio = new VBox(tituloBox,jugarBox);
+		
+		Scene scene = new Scene(contenedorInicio,400,400);
 		return scene;
 	}
 	
